62ab73c119e30c77b50d675845519967f4654cae
Archishman-Naskar <archishmannskr2005@gmail.com>
2026-03-13 15:22:58 +0530

    Implemented Update profile

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { getUserProfile, myProfile } from "../controllers/user.js";
+import {
+  getUserProfile,
+  myProfile,
+  updateUserProfile,
+} from "../controllers/user.js";
 import { isAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/me", isAuth, myProfile);
 router.get("/:userId", isAuth, getUserProfile);
+router.put("/update/profile", isAuth, updateUserProfile);
 
 export default router;
